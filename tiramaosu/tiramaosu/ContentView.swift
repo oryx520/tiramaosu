5485cbea63f2f2c4e5e2344f90ee38c37c62ba85
oryx520 <vyx520@gmail.com>
2026-04-07 19:37:18 -0400

    2

---
+++ tiramaosu/tiramaosu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tiramaosu
+//
+//  Created by vyx on 2026-04-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tiramaosu/tiramaosu/tiramaosuApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiramaosuApp.swift
+//  tiramaosu
+//
+//  Created by vyx on 2026-04-07.
+//
+
+import SwiftUI
+
+@main
+struct tiramaosuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
